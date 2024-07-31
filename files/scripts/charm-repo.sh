783682464f763c16fafebcc84876c5e4c5e15400
DarkXero-dev <steve@techxero.com>
2024-07-31 14:12:06 +0300

    Fingers crossed Commit

--- files/scripts/charm-repo.sh
+++ files/scripts/charm-repo.sh
@@ -12,3 +12,6 @@ baseurl=https://repo.charm.sh/yum/
 enabled=1
 gpgcheck=1
 gpgkey=https://repo.charm.sh/yum/gpg.key" > /etc/yum.repos.d/charm.repo
+
+FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION:-40}"
+curl -Lo /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${FEDORA_MAJOR_VERSION}"/che-nerd-fonts-fedora-"${FEDORA_MAJOR_VERSION}".repo
