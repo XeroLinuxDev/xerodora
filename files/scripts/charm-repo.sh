8228f4f2c7c486e61ac41ad41362efb8a29ecfa9
DarkXero-dev <steve@techxero.com>
2024-07-31 13:25:20 +0300

    Repo added

---
+++ files/scripts/charm-repo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo "[charm]
+name=Charm
+baseurl=https://repo.charm.sh/yum/
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.charm.sh/yum/gpg.key" > /etc/yum.repos.d/charm.repo
