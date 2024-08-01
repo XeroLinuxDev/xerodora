947072ae7c46dc935b5e4488a10432250dee7814
DarkXero-dev <steve@techxero.com>
2024-08-01 13:48:06 +0300

    Test updates

--- files/scripts/repos.sh
+++ files/scripts/repos.sh
@@ -5,14 +5,6 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-# Charm Repo.
-echo "[charm]
-name=Charm
-baseurl=https://repo.charm.sh/yum/
-enabled=1
-gpgcheck=1
-gpgkey=https://repo.charm.sh/yum/gpg.key" > /etc/yum.repos.d/charm.repo
-
 # Nerd-Fonts Repo
 FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION:-40}"
 curl -Lo /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${FEDORA_MAJOR_VERSION}"/che-nerd-fonts-fedora-"${FEDORA_MAJOR_VERSION}".repo
